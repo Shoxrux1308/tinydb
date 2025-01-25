aaeb77b69f4f309aa606c748dd1d78b767ad6c07
Shoxrux1308 <shohruxb1308@gmail.com>
2025-01-25 19:34:14 +0500

    create database

---
+++ main.py
@@ -0,0 +1,5 @@
+from tinydb import TinyDB
+
+db = TinyDB('db.json',indent=4)
+db.insert({'name': 'Abdumajid',"lastname":"Abdulazizov", "job":"Student"})
+db.insert({'name': 'Mehroj',"lastname":"abdullayev" ,"job":"Developer"})
