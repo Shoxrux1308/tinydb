e7302494c045863d05c9ff314b868bc53717bd5b
Shoxrux1308 <shohruxb1308@gmail.com>
2025-01-25 19:35:45 +0500

    insert doucments'

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tinydb import TinyDB
 
-db = TinyDB('db.json',indent=4)
+db = TinyDB('db.json', indent=4)
+
 db.insert({'name': 'Abdumajid',"lastname":"Abdulazizov", "job":"Student"})
 db.insert({'name': 'Mehroj',"lastname":"abdullayev" ,"job":"Developer"})
