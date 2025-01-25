37b3c9830869efb809f0f867c09b6652c0a73271
Shoxrux1308 <shohruxb1308@gmail.com>
2025-01-25 19:56:00 +0500

    create randomuser

--- randomuser.py
+++ randomuser.py
@@ -1,4 +1,9 @@
 import requests
 from tinydb import TinyDB
 
-db=TinyDB('randomuser.json')
+db=TinyDB('randomuser.json',indent=4)
+
+response=requests.get('https://randomuser.me/api/?results=5')
+user=response.json()["results"]
+for i in user:
+    db.insert({"name":i["name"]["first"],"last_name":i["name"]["last"],"age":i["dob"]["age"],"gender":i["gender"]})  
