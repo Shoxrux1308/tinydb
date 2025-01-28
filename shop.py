8a97a942fe56b1fc8ed6ad752d770643ab212c6f
Shoxrux1308 <shohruxb1308@gmail.com>
2025-01-28 13:46:44 +0500

    create table

--- shop.py
+++ shop.py
@@ -1,3 +1,6 @@
 from tinydb import TinyDB
 
-db=TinyDB('shop_db.json')
+db=TinyDB('shop_db.json',indent=4)
+db.insert({'name':'Macbook Pro','price':1800,'color':'Space Grey','memory':256})
+db.insert({'name':'iPhone 15 Pro','price':1400,'color':'Black','memory':512})
+db.insert({'name':'Samsung Galaxy S24 Ultra','price':1200,'color':'Black','memory':128})
